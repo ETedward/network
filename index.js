bde55bd3f31bf54481aa092f46da4052f5fbe3c7
Edward Tian <edwardtian2000@gmail.com>
2020-07-07 10:53:01 -0400

    index

--- index.js
+++ index.js
@@ -1,20 +1,699 @@
-var width = 960,
-    height = 500;
+// d3.csv("data/users.csv", function(data) {
+//     var canvas = d3.select(".importData").append("svg")
+//     console.log(data[0])
+// }
 
-var links = [
-    { source: 'Bara', target: 'Lann'},
-    { source: 'Bara', target: 'Star'},
-    { source: 'Lann', target: 'Star'},
-    { source: 'Star', target: 'Home'},
-    { source: "alo", target: 'ola'},
-    { source: "ola", target: 'alo'},
-    { source: "tedros", target: "Lann"},
-    { source: "tedros", target: "Bara"},
-    { source: "tedros", target: "Star"},
-    { source: "tedros", target: "Home"},
-    { source: "tedros", target: "alo"},
-    { source: "tedros", target: "ola"},
-];
+
+// var request = new XMLHttpRequest();
+// request.open("GET", "connections.json", false);
+// request.send(null)
+//var links = JSON.parse(request.responseText)
+// var links = JSON.parse(
+//     {
+//       "source": "jonas_jewels",
+//       "target": "standwi78914908"
+//     },
+// )
+
+var links  = [
+    {
+      "source": "jonas_jewels",
+      "target": "standwi78914908"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "bensolohk"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "george_of_fl"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "vianaremy"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "yohannesggetah1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "highpickers"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "tibetpeople"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "drwaseemshaukat"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "trvstworld"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "michael_jsph94"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "junlee520"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "poppyjonah"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "uma_julian"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "sydney35774681"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "namra9496"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "lunaid05"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "dochaney"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "aloksingh_"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "nesk2020"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "rocklee31855449"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "zackpeckinpah"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "delawaredave"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "libroaster4life"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "biologistrobert"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "frankly_lam"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "skinny8product1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "dabenzhongus01"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "mingchuanwang1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "asmodeusjust"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "eric501373"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "chrishlook"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "oogidoogiboogi"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "babbabang"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "standwi78914908"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "bensolohk"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "george_of_fl"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "vianaremy"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "yohannesggetah1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "highpickers"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "tibetpeople"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "drwaseemshaukat"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "trvstworld"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "michael_jsph94"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "junlee520"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "poppyjonah"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "uma_julian"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "sydney35774681"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "namra9496"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "lunaid05"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "dochaney"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "aloksingh_"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "nesk2020"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "rocklee31855449"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "zackpeckinpah"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "delawaredave"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "libroaster4life"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "biologistrobert"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "frankly_lam"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "skinny8product1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "dabenzhongus01"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "mingchuanwang1"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "asmodeusjust"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "eric501373"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "chrishlook"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "oogidoogiboogi"
+    },
+    {
+      "source": "jonas_jewels",
+      "target": "babbabang"
+    },
+    {
+      "source": "babbabang",
+      "target": "newsnadykett191"
+    },
+    {
+      "source": "babbabang",
+      "target": "flavinkins"
+    },
+    {
+      "source": "babbabang",
+      "target": "trvstworld"
+    },
+    {
+      "source": "babbabang",
+      "target": "raconpreza"
+    },
+    {
+      "source": "babbabang",
+      "target": "poppyjonah"
+    },
+    {
+      "source": "babbabang",
+      "target": "uma_julian"
+    },
+    {
+      "source": "babbabang",
+      "target": "sonu9910"
+    },
+    {
+      "source": "babbabang",
+      "target": "bcpbf_official"
+    },
+    {
+      "source": "babbabang",
+      "target": "rocklee31855449"
+    },
+    {
+      "source": "babbabang",
+      "target": "antony23832972"
+    },
+    {
+      "source": "babbabang",
+      "target": "feminismandfre1"
+    },
+    {
+      "source": "babbabang",
+      "target": "aadarshkothari"
+    },
+    {
+      "source": "babbabang",
+      "target": "kesa2004"
+    },
+    {
+      "source": "babbabang",
+      "target": "libroaster4life"
+    },
+    {
+      "source": "babbabang",
+      "target": "biologistrobert"
+    },
+    {
+      "source": "babbabang",
+      "target": "frankly_lam"
+    },
+    {
+      "source": "babbabang",
+      "target": "257746331ok"
+    },
+    {
+      "source": "babbabang",
+      "target": "chiragasodaria"
+    },
+    {
+      "source": "babbabang",
+      "target": "aislamophobes"
+    },
+    {
+      "source": "babbabang",
+      "target": "maxndj4"
+    },
+    {
+      "source": "babbabang",
+      "target": "fifthtwiaccount"
+    },
+    {
+      "source": "babbabang",
+      "target": "anotherxhater"
+    },
+    {
+      "source": "babbabang",
+      "target": "jonas_jewels"
+    },
+    {
+      "source": "babbabang",
+      "target": "dabenzhongus01"
+    },
+    {
+      "source": "babbabang",
+      "target": "wattscalif"
+    },
+    {
+      "source": "babbabang",
+      "target": "vamsi1102gmail1"
+    },
+    {
+      "source": "babbabang",
+      "target": "skander362343"
+    },
+    {
+      "source": "babbabang",
+      "target": "botsentinel111"
+    },
+    {
+      "source": "babbabang",
+      "target": "rusmiza9"
+    },
+    {
+      "source": "babbabang",
+      "target": "lindaartwork"
+    },
+    {
+      "source": "babbabang",
+      "target": "eric501373"
+    },
+    {
+      "source": "babbabang",
+      "target": "chrishlook"
+    },
+    {
+      "source": "babbabang",
+      "target": "asmodeusjust"
+    },
+    {
+      "source": "babbabang",
+      "target": "oogidoogiboogi"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "belldanielj"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "wolvesgang2"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "zuhalbayrakkk"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "sharma02932939"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "feranewlife"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "sn923127884"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "poppyjonah"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "uma_julian"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "iyejah"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "gy9mk0c1u3gxe3i"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "cyndi63079794"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "nadaburger"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "ahmed_4846"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "sakariaalugodhi"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "justanastrogirl"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "asmodeusjust"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "mrshegberg"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "amaratungagehan"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "flavinkins"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "geminiseventh"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "rgrzdaughter"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "its_esank"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "andre77826436"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "mettarobbi"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "kurtdrago"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "sinmix"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "portenacalg"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "btcdca"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "feligonzaesco"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "ukimostik"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "karaer007"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "berhane_meskel"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "andrewm23786363"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "yozz666"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "rooneynutmeg"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "nossahsj"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "admiral_jkirk"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "simply_love_all"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "lyinghealer"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "picazo28690559"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "rocklee31855449"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "enthusiastproof"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "adolfomahla"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "juliabemer"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "ianebow"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "paulineflot"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "gtpatel"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "andoid64"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "nadiahuet2"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "bagherra42"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "itouchedmyface"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "robberducky00"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "gq802"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "tarenpang"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "yangyi54771288"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "shawnlo94790920"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "hughlesman"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "nomdeplumi1"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "magpies1314"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "danielnshaba"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "2mtusks"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "angiebaby503"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "eric501373"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "babbabang"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "jonas_jewels"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "oogidoogiboogi"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "chrishlook"
+    },
+    {
+      "source": "BiologistRobert",
+      "target": "joejmg0826"
+    },
+    
+  ]
+
+var width = 1400,
+    height = 800;
+//var links = data.links.map(d => Object.create(d));
 
 var nodes = {} // parse links to nodes
 links.forEach(function(link){ // for loop
@@ -49,7 +728,7 @@ var node = svg.selectAll('.node')
     .data(force.nodes())
     .enter().append("circle")
     .attr("class","node")
-    .attr('r',width * 0.01);
+    .attr('r',width * 0.005);
 
 var text = svg.selectAll('.text')
     .data(force.nodes())
