d47b0f9b9f553f51875df8ba4b28d86526f09d02
Edward Tian <edwardtian2000@gmail.com>
2020-07-13 23:49:27 -0400

    index

--- index.js
+++ index.js
@@ -733,7 +733,7 @@ var node = svg.selectAll('.node')
 var text = svg.selectAll('.text')
     .data(force.nodes())
     .enter().append("text")
-    .text("Tedros Meme Bots Network")
+    .text("Tedros Network")
     .attr("y", function(d, i) {
         return 20;
     })
